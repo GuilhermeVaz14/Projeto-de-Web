a7e038a63bf45527c710877d0a90e42e9eb8190f
VazGuilherme14 <Vazguilherme28@gmail.com>
2024-12-18 11:58:54 -0300

    JavaScript Finalizado

--- script.js
+++ script.js
@@ -19,19 +19,19 @@ const perguntas = [
     {
         pergunta: "Qual é o melhor passatempo?",
         opcoes: [
-            { texto: "Jogar golfe", personagem: "Kelvin", pontos: 10 },
-            { texto: "Tocar guitarra", personagem: "Stuart", pontos: 10 },
             { texto: "Ouvir histórias", personagem: "Bob", pontos: 10 },
-            { texto: "Lançar foguetes", personagem: "Dave", pontos: 10 }
+            { texto: "Tocar guitarra", personagem: "Stuart", pontos: 10 },
+            { texto: "Lançar foguetes", personagem: "Dave", pontos: 10 },
+            { texto: "Jogar golfe", personagem: "Kelvin", pontos: 10 }
         ],
         imagemFundo: "https://down-br.img.susercontent.com/file/eda11e9f3a1adf6c98e219483624316b"
     },
     {
         pergunta: "Qual melhor representa seu cabelo?",
         opcoes: [
-            { texto: "Bagunçado", personagem: "Kelvin", pontos: 10 },
-            { texto: "Penteado", personagem: "Stuart", pontos: 10 },
             { texto: "Careca", personagem: "Bob", pontos: 10 },
+            { texto: "Penteado", personagem: "Stuart", pontos: 10 },
+            { texto: "Bagunçado", personagem: "Kelvin", pontos: 10 },
             { texto: "Repartido ao meio", personagem: "Dave", pontos: 10 }
         ],
         imagemFundo:"https://static.jogalo.com/i/133a816.jpg"
@@ -39,9 +39,9 @@ const perguntas = [
     {
         pergunta: "Qual olho você gostaria de ter?",
         opcoes: [
-            { texto: "Um par de olhos", personagem: "Kelvin", pontos: 10 },
-            { texto: "Apenas um", personagem: "Stuart", pontos: 10 },
             { texto: "Um de cada cor", personagem: "Bob", pontos: 10 },
+            { texto: "Apenas um", personagem: "Stuart", pontos: 10 },
+            { texto: "Um par de olhos", personagem: "Kelvin", pontos: 10 },
             { texto: "Qualquer um", personagem: "Dave", pontos: 10 }
         ],
         imagemFundo: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhCtmnpVJLmFrLcOjRH7Zx2q65NuItUh2yqGuNLnOFJ4NVHddvR-LTBb_OoAGC3zdP9CP9tx2ovV9Rralc22JYdtqBACafjfmdKwrhafzNrmUaNGdeEuHtGcZJDMDQttatPwBtISVquwOFD/s16000/1654871359070549-0.png"
@@ -49,29 +49,29 @@ const perguntas = [
     {
         pergunta: "Defina seu MOOD do dia!",
         opcoes: [
+            { texto: "Foco na meta", personagem: "Dave", pontos: 10 },
             { texto: "Vivendo a vida", personagem: "Kelvin", pontos: 10 },
             { texto: "De mal com a vida", personagem: "Stuart", pontos: 10 },
-            { texto: "De love", personagem: "Bob", pontos: 10 },
-            { texto: "Foco na meta", personagem: "Dave", pontos: 10 }
+            { texto: "De love", personagem: "Bob", pontos: 10 }
         ],
         imagemFundo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlisHPBu5sGA8GRi3-ij1cTThTtryvAU-Apw&s"
     },
     {
         pergunta: "Você gosta de BANANA?",
         opcoes: [
-            { texto: "Gosto", personagem: "Kelvin", pontos: 10 },
-            { texto: "Gosto muito", personagem: "Stuart", pontos: 10 },
             { texto: "Adoro BANANAS", personagem: "Bob", pontos: 10 },
-            { texto: "Sim", personagem: "Dave", pontos: 10 }
+            { texto: "Sim", personagem: "Dave", pontos: 10 },
+            { texto: "Gosto", personagem: "Kelvin", pontos: 10 },
+            { texto: "Gosto muito", personagem: "Stuart", pontos: 10 }
         ],
         imagemFundo:"https://m.media-amazon.com/images/I/91dXVSVtxyL._AC_UF1000,1000_QL80_.jpg"
     },
     {
         pergunta: "Qual seu personagem favorito de Minions?",
         opcoes: [
+            { texto: "Agnes", personagem: "Bob", pontos: 10 },
             { texto: "Margô", personagem: "Kelvin", pontos: 10 },
             { texto: "Edith", personagem: "Stuart", pontos: 10 },
-            { texto: "Agnes", personagem: "Bob", pontos: 10 },
             { texto: "Dr. Nefario", personagem: "Dave", pontos: 10 }
         ],
         imagemFundo: "https://live.staticflickr.com/3735/9790005014_2c8f2d7b9b_b.jpg" 
@@ -79,10 +79,10 @@ const perguntas = [
     {
         pergunta: "Qual melhor representa seu temperamento?",
         opcoes: [
-            { texto: "Fleumático", personagem: "Kelvin", pontos: 10 },
             { texto: "Sanguíneo", personagem: "Stuart", pontos: 10 },
-            { texto: "Colérico", personagem: "Bob", pontos: 10 },
-            { texto: "Melancólico", personagem: "Dave", pontos: 10 }
+            { texto: "Fleumático", personagem: "Kelvin", pontos: 10 },
+            { texto: "Melancólico", personagem: "Dave", pontos: 10 },
+            { texto: "Colérico", personagem: "Bob", pontos: 10 }
         ],
         imagemFundo:"https://zonagamer.com.br/wp-content/uploads/2024/07/Meu-Malvado-Favorito-4-esquece-que-Minions-sao-um-tempero.jpg"
     },
@@ -90,26 +90,25 @@ const perguntas = [
         pergunta: "Você é responsável?",
         opcoes: [
             { texto: "Sim, o mais responsável", personagem: "Kelvin", pontos: 10 },
-            { texto: "Não, sou muito bagunceiro(a)", personagem: "Stuart", pontos: 10 },
             { texto: "Não", personagem: "Bob", pontos: 10 },
-            { texto: "Sim", personagem: "Dave", pontos: 10 }
+            { texto: "Sim", personagem: "Dave", pontos: 10 },
+            { texto: "Não, sou muito bagunceiro(a)", personagem: "Stuart", pontos: 10 }
         ],
         imagemFundo:"https://izaeventos.wordpress.com/wp-content/uploads/2017/04/decoracao-de-aniversario-em-caucaia-2-minions.jpg?w=750"
     },
     {
         pergunta: "Qual sua sobremesa favorita?",
         opcoes: [
-            { texto: "Pedra", personagem: "Kelvin", pontos: 10 },
             { texto: "Shakes", personagem: "Stuart", pontos: 10 },
             { texto: "Cereal", personagem: "Bob", pontos: 10 },
-            { texto: "Sorvete", personagem: "Dave", pontos: 10 }
+            { texto: "Sorvete", personagem: "Dave", pontos: 10 },
+            { texto: "Pedra", personagem: "Kelvin", pontos: 10 }
         ],
         imagemFundo:"https://cloudfront-us-east-1.images.arcpublishing.com/estadao/PSIL3SGU2FNILBWFJPXX2TWLGE.jpg"
     }
 ];
 
-
-let PerguntaAtual = 0; 
+let PerguntaAtual = 0;
 
 function carregarPergunta() {
     const pergunta = perguntas[PerguntaAtual];
@@ -120,7 +119,6 @@ function carregarPergunta() {
 
     perguntaElemento.textContent = pergunta.pergunta;
     numeroPergunta.textContent = `Pergunta #${PerguntaAtual + 1}`;
-
     fundoFoto.innerHTML = `<img src="${pergunta.imagemFundo}" width="100%" height="100%">`;
 
     opcoesElemento.innerHTML = "";
@@ -143,7 +141,6 @@ function carregarPergunta() {
         div.appendChild(label);
         opcoesElemento.appendChild(div);
     });
-    perguntaFundo.style.backgroundImage = `url('${pergunta.imagemFundo}')`;
 }
 
 function RegistrarEscolhas() {
@@ -181,8 +178,8 @@ function ProximaPergunta() {
 }
 
 function mostrarResultado() {
-    const resultado = document.getElementById("resultado");
-
+    const totalPerguntas = perguntas.length; 
+    const maximoPontos = totalPerguntas * 10; 
     let maiorPontuacao = 0;
     let personagemFinal = "";
 
@@ -193,10 +190,14 @@ function mostrarResultado() {
         }
     }
 
-    resultado.textContent = `Você é: ${personagemFinal}!`;
-    document.getElementById("opcoes").innerHTML = ""; 
-    document.getElementById("pergunta").textContent = "Fim do quiz!";
-    document.querySelector("button").style.display = "none"; 
-}
+    const percentual = Math.round((maiorPontuacao / maximoPontos) * 100); 
+    localStorage.setItem("personagemFinal", personagemFinal);
+    localStorage.setItem("pontuacaoFinal", percentual);
 
+    if (personagemFinal) {
+        window.location.href = `${personagemFinal.toLowerCase()}.html`;
+    } else {
+        alert("Erro ao calcular o resultado!");
+    }
+}
 carregarPergunta();
